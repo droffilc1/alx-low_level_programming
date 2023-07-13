e2757fe47cd399607cea5e8c70f1b02b5088502b
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-07-13 19:01:27 +0300

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/*
- * * main: prints a string using the puts function 
+/**
+ * main: Prints a string using the puts function
+ *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
