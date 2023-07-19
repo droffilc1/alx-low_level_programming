703dd4b7e651e4b3123569bebfa36334c801549d
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-07-19 14:13:26 +0300

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - makes sum of two numbers
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: The sum of the two parameters
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
