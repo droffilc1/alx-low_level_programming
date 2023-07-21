ab9d7beb6dc03fa9df81bf680615cbde14e6aac5
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-07-21 12:59:06 +0300

    Prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Finds and prints the largest prime factors of 
+ * the number 612852475143, followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	long test_num = 612852475143;
+	long divisor = 2;
+
+	while (divisor != test_num)
+	{
+		if (test_num % 2 == 0)
+		{
+			test_num /= divisor;
+		}
+		else
+		{
+			divisor++;
+		}
+	}
+	printf("%ld\n", test_num);
+
+	return (0);
+}
