2034b7991aa474611ad1e9d5cc8b1efe395879d0
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-08-02 19:53:08 +0300

    Add solution to task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,13 +9,13 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	while (s[i])
+	if (*s == '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar('\n');
 	}
-	_putchar('\n');
-
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
 }
