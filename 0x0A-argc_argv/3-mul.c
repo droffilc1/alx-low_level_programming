da22c3eeaf333b6e03aec22de1932651353d5e8d
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-08-05 03:55:27 +0300

    Add solution for task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  print the result of the multiplication
+ * @argc: The number of arguments
+ * @argv: An array of strings
+ *
+ * Return: 0. Otherwise 1
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc > 2)	
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
