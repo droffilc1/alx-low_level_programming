ac849aa7b81ca37dd85d86ac6e92386bc954fb01
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-08-08 20:43:45 +0300

    Add solution to task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - concatenates all the arguments
+ * @ac: The  first argument
+ * @av: The second argument
+ *
+ * Return: pointer to a new string. Otherwise NULL.
+ */
+char *argstostr(int ac, char **av)
+{
+	int i;
+	size_t len = 0;
+	char *_all_args, *all_args;
+
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		len += strlen(av[i]);
+	}
+
+	len += ac - 1 + 1;
+
+	_all_args = all_args = malloc(len);
+
+	for (i = 0; i < ac; i++)
+	{
+		memcpy(_all_args, av[i], strlen(av[i]));
+		_all_args += strlen(av[i]);
+
+		if (i < ac - 1)
+		{
+			*_all_args = '\n';
+			_all_args++;
+		}
+		else
+		{
+			*_all_args = '\n';
+			_all_args++;
+			*_all_args = '\0';
+		}
+	}
+
+
+	return (all_args);
+
+	free(all_args);
+}
