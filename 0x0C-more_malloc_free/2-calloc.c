c18ba55a5d48dda8f81eaa2bd0a33c47b40e147e
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-08-10 21:49:04 +0300

    Update solution to task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to the destination object
+ * @b: value to be filled
+ * @n: number of bytes to be filled
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *p = s;
+
+	while (n--)
+	{
+		*p++ = b;
+	}
+
+	return (s);
+}
+
 
 /**
  * _calloc - allocates memory for an array, using malloc
@@ -10,16 +32,18 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int *ptr = NULL;
+	void *ptr = NULL;
+	unsigned int total_size = nmemb * size;
 
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(sizeof(unsigned int) * size);
+	ptr = malloc(total_size);
 
 	if (ptr == NULL)
 		return (NULL);
+	_memset(ptr, 0, total_size); /*Initialize memory to zero */
 
 	return (ptr);
 }
