c0a8c2fd12878fd1cf297b0461b222cf947f90f0
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-08-14 18:25:25 +0300

    Add solution to task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure of a dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: structure of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
