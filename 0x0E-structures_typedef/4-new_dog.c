fe25970334b47a64a8017aadfc4518cad64d9973
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-08-15 13:19:19 +0300

    Add solution to task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,48 @@
 #include "dog.h"
 
 /**
+ * _strlen - returns length of a string
+ * @s: The characters to be checked
+ *
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * _strcpy - copies the string pointed to by src, including
+ * the terminating null byte (\0), to the buffer pointed to by dest
+ * @dest: The destination
+ * @src: The copied
+ *
+ * Return: The pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *new = dest;
+
+	while(*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (new);
+}
+
+/**
  * new_dog - creates new dog
  * @name: The name member
  * @age: The age member
@@ -16,8 +58,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 		return (NULL);
-	ptr->name = name;
+
+	ptr->name = malloc(_strlen(name) + 1);
+	ptr->owner = malloc(_strlen(owner) + 1);
+
+	_strcpy(ptr->name, name);
+	_strcpy(ptr->owner, owner);
+
 	ptr->age = age;
-	ptr->owner = owner;
+
 	return (ptr);
 }
