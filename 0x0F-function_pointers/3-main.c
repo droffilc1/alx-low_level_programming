f6f833d5f2e8093f3e75d062603960cae3e2ff01
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-08-17 02:35:20 +0300

    Add solution to task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,1 +1,33 @@
-main
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - perform simple operations
+ *@argc: arguments
+ *@argv: array of arguments
+ *
+ * Return: integer
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 4)
+	{
+		int a, b;
+
+		int (*func)(int, int);
+
+		a = atoi(argv[1]);
+		b = atoi(argv[3]);
+		func = get_op_func(argv[2]);
+		if ((*argv[2] == '%' || *argv[2] == '/') && b == 0)
+		{
+			printf("Error\n");
+			exit(100);
+		}
+		printf("%d\n", func(a, b));
+		return (0);
+	}
+	printf("Error\n");
+	exit(98);
+}
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,57 @@
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+#include <stdio.h>
+
+/**
+ * op_add - sum of two nums
+ * @a: The first operand
+ * @b: The second operand
+ *
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub- subtracts of two nums
+ * @a: The first operand
+ * @b: The second operand
+ *
+ * Return: subtraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplication of two nums
+ * @a: The first operand
+ * @b: The second operand
+ *
+ * Return: result of mul
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides of two nums
+ * @a: The first operand
+ * @b: The second operand
+ *
+ * Return: result of division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modm of two nums
+ * @a: The first operand
+ * @b: The second operand
+ *
+ * Return: result of mod
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
