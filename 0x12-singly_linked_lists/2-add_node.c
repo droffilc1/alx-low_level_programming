44551627d4641fde810678452c6b9902a6f1ae89
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-08-25 00:10:04 +0300

    Update solution to task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,15 +14,15 @@ list_t *add_node(list_t **head, const char *str)
 	if (head == NULL || str == NULL)
 		return (NULL);
 
-	if (head == NULL || str == NULL)
-		return (NULL);
-
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
 	if (new->str == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 	new->len = strlen(str);
 	new->next = *head;
 	*head = new;
