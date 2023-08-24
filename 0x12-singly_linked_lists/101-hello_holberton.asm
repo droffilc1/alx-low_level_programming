4474b37e19ab82c85ad783e71402e1ae2c5ce1f1
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-08-25 01:42:44 +0300

    Update solution to task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,15 @@
-section .data
-	hello db "Hello, Holberton", 10, 0 ;string to be printed (null terminated)
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-section .text
-	global main
+	SECTION .text
 	extern printf
-
+	global main
 main:
-	sub rsp, 8		; Align the stack (16-byte alignment)
-	lea rdi, [hello]	; Load the address of the string into rdi
-	call printf		; Call the printf function
-	add rsp, 8		; Adjust the stack pointer after the call
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-	;Exit the program
-	mov rax, 60		;syscall number for sys_exit
-	xor rdi, rdi		; exit status 0
-	syscall
-
+	mov eax, 0
+	ret
