50f051de20aabd6e9198d1f2c88bdfb4361eaec9
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-08-25 00:54:09 +0300

    Add solution to task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_string - prints a string
+ *
+ * Return: 0 (Success).
+ */
+int print_string(void)
+{
+	printf("You're beat! and yet, you must allow,
+		\nI bore my house upon my back!\n");
+	return (0);
+}
---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int print_string(void);
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_string();
+	printf("(A tortoise, having pretty good sense of a hare's nature,
+		challenges one to a race.)\n");
+	return (0);
+}
