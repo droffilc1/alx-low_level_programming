928d49ec1ecc0da615c90223bd8bcc0b928dcb48
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-08-28 18:25:20 +0300

    Add solution to task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - number of elements in a linked list
+ * @h: list
+ *
+ * Return: number of elements in a linked list
+ */
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
