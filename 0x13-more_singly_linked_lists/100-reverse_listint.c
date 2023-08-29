485e6248b786916c8c3f8e84ad03b6b960dc36c6
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-08-29 19:15:11 +0300

    Update solution to task 11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,13 +11,14 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *prev = NULL;
 	listint_t *next_node = NULL;
 
-	while (!head)
+	while (*head != NULL)
 	{
 		next_node = (*head)->next;
 		(*head)->next = prev;
 		prev = *head;
 		*head = next_node;
-		*head = prev;
 	}
+	*head = prev;
+
 	return (*head);
 }
