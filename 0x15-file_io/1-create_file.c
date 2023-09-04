22e2eaadd65af9fbbc95aa58534c18e539086bc3
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-09-05 02:09:59 +0300

    Add solution to task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file to create
+ * @text_content: a NULL terminated string
+ *
+ *
+ * Return: 1 (Success). Otherwise -1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, checkw, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[len] != '\0')
+			len++;
+		checkw = write(fd, text_content, len);
+
+		if (checkw == -1)
+			return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
