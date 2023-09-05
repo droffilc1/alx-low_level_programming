e951bbad0bff587b4a95fa200afc39df30a7e678
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-09-05 10:35:49 +0300

    Add solution to task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _strlen - finds length of a string
+ * @s: string to be checked
+ *
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of the file
+ * @text_content: NULL terminated string
+ *
+ * Return: 1 (Success). Otherwise -1.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int checkw;
+
+	if (filename == NULL || text_content == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	checkw = write(fd, text_content, _strlen(text_content));
+
+	if (checkw == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
