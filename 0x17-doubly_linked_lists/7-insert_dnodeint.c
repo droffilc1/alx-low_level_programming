a0916e7e3a01892f5e65ec586803d5ee1b687c71
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-10-14 11:30:09 +0300

    fix: update solution to task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,8 +25,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	if (current && current->next == NULL)
-		return (add_dnodeint(h, n));
-	
+		return (add_dnodeint_end(h, n));
+
 	if (current != NULL)
 	{
 		/* Allocate memory for new node */
