d592d667bd377f102adf4477e9e4864a403eccd4
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-10-16 16:15:46 +0300

    feat: add solution to task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,63 @@
+#include "lib.h"
+
+/**
+ * add - adds two integers
+ * @a: The first operand
+ * @b: The second operand
+ *
+ * Return: sum of two integers
+ */ 
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subracts two numbers
+ * @a: The first operand
+ * @b: The second operand
+ *
+ * Return: result of subraction of two numbers
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two numbers
+ * @a: The first operand
+ * @b: The second operand
+ *
+ * Return: mul of two numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - division of two numbers
+ * @a: The first operand
+ * @b: The second operand
+ *
+ * Return: division of to numbers
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus of two numbers
+ * @a: The first operand
+ * @b: The second operand
+ *
+ * Return: modulus of two numbers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
