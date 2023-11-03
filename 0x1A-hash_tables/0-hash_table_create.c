ab328f95e23a2f3f46aaeb919d1a866baa52ae84
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-11-03 07:23:22 +0300

    feat: add solution to task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_create - creates a hash table.
+* @size: size of the array
+*
+* Return: A pointer to the newly created hash table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(size * sizeof(hash_table_t));
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	i = 0;
+	while (i < size)
+	{
+		ht->array[i] = NULL;
+		i++;
+	}
+
+	return (ht);
+}
