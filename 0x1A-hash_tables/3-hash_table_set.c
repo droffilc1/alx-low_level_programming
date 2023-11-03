129c2bd3b9ff7ec96a6c60744e80b0ce54f689f2
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-11-03 09:39:41 +0300

    feat: add solution to task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_set - adds an element to the hash table
+* @ht: hash table to add an element
+* @key: the key
+* @value: value associated with the key
+*
+* Return: 1 (Success). Otherwise 0.
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int idx, i;
+
+	if (!ht || !key || *key == '\0' || !value)
+		return (0);
+
+	value_copy = strdup(value);
+	if (!value_copy)
+		return (0);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	for (i = idx; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->value, key) == 0)
+		{
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (!new->key)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
+
+	return (1);
+}
